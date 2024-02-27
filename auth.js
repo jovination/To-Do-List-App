0d4795023eb82ff3ca43b6eabf7e1d82f88c98e1
jovination <jovination791@gmail.com>
2024-02-27 22:37:00 +0300

    adding passport packages

--- auth.js
+++ auth.js
@@ -4,10 +4,16 @@ require('dotenv').config();
 passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://locolhost:5100/auth/google/callback",
+    callbackURL: "http://localhost:5100/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
     done(null, user);
     }
 ));
+passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+ passport.deserializeUser((user, done) => {
+    done(null, user);
+  });
