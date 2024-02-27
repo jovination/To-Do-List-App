7e8f640ff4657823bf225a2867b1478f196d033f
jovination <jovination791@gmail.com>
2024-02-28 01:10:15 +0300

    fw changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const passport = require('passport');
+const session = require('express-session');
 require('./auth')
 
 app.set('view engine', 'ejs')
@@ -16,9 +17,26 @@ app.get('/auth/google',
 
 app.get( '/auth/google/callback',
     passport.authenticate( 'google', {
-        successRedirect: '/auth/google/success',
+        successRedirect: '/auth/protected',
         failureRedirect: '/auth/google/failure'
 }));
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect('/auth/google');
+}
+app.get('/auth/protected',(req, res) => {
+    res.render('protected')
+});
+
+app.use(session({
+    secret: 'mysecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
 
 app.use(express.static(__dirname + '/public'));
 app.listen(process.env.PORT || 5100);
