73b4bfcaf7f8d48ed9df51017cb53af61fc067b2
jovination <jovination791@gmail.com>
2024-02-27 23:10:51 +0300

    few updates

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
 require('./auth')
 
 app.set('view engine', 'ejs')
@@ -7,6 +8,17 @@ app.get('/', (req, res) => {
  res.render('index')
 
 })
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope:
+      [ 'email', 'profile' ] }
+));
+
+app.get( '/auth/google/callback',
+    passport.authenticate( 'google', {
+        successRedirect: '/auth/google/success',
+        failureRedirect: '/auth/google/failure'
+}));
 
 app.use(express.static(__dirname + '/public'));
 app.listen(process.env.PORT || 5100);
