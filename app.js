c7f2d3a1c66607dbaaa21dd8050eedfa1bb78f79
jovination <jovination791@gmail.com>
2024-03-03 13:54:51 +0300

    few changes

--- app.js
+++ app.js
@@ -58,8 +58,8 @@ app.get('/logout', (req, res) => {
     // Destroy user session
     req.logout();
     // Redirect user to localhost:5100
-   // res.redirect('https://to-do-lyst.onrender.com');
-    res.redirect('http//localhost:5100');
+     res.redirect('https://to-do-lyst.onrender.com');
+   // res.redirect('http//localhost:5100');
 
 });
 
