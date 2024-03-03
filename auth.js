f8b8b54e3f4ede20f471a88cd60ae88be583f635
jovination <jovination791@gmail.com>
2024-03-03 13:20:49 +0300

    few updates

--- auth.js
+++ auth.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://to-do-lyst.onrender.com/auth/google/callback",
+    callbackURL: "https://to-do-lyst.onrender.com/auth/google/callback",
     passReqToCallback: true
   },
   function(request, accessToken, refreshToken, profile, done) {
