aea1fe01684594854b922d75e2520815f16c9282
jovination <jovination791@gmail.com>
2024-04-24 14:27:21 +0300

    changes on header

--- auth.js
+++ auth.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-   callbackURL: "https://to-do-lyst.onrender.com/auth/google/callback",
-    //callbackURL: "http://localhost:5100/auth/google/callback",
+   //callbackURL: "https://to-do-lyst.onrender.com/auth/google/callback",
+    callbackURL: "http://localhost:5100/auth/google/callback",
 
     passReqToCallback: true
   },
